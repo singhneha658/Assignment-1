8d41094aa6280030dedfb92de05f327846eb023a
singhneha658 <108941665+singhneha658@users.noreply.github.com>
2022-07-24 22:37:33 +0530

    Add files via upload

---
+++ tuple_sort.py
@@ -0,0 +1,11 @@
+#Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+
+def last(n):
+    return n[-1] 
+  
+def sort(tuples):
+    return sorted(tuples, key=last)
+  
+a=[(1, 3), (3, 2), (2, 1),(5,4),(7,6)]
+print("Sorted:")
+print(sort(a))
